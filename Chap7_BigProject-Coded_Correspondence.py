075900cdf51877bd8ff04f9ff136ae7aa670e192
Kenneth B. Bjerke <kenneth.bjerke@gmail.com>
2022-02-13 22:55:57 +0100

    Add files via upload

--- Chap7_BigProject-Coded_Correspondence.py
+++ Chap7_BigProject-Coded_Correspondence.py
@@ -72,5 +72,39 @@ coded_message = "vhfinmxkl atox kxgwxkxw tee hy maxlx hew vbiaxkl tl hulhexmx. p
 for i in range(1, 26):
 	print("Offset: " + str(i))
 	print("\t " + decoder(coded_message, i) + "\n")
+	
+	
+# Part 6 - This one getting rough. Here we might need to watch the solution (again, already had to do it a bit...)
+# This is a new system we have to crack. Create a function that takes two parameters, the message and a keyword. 
 
+# he Vigenère Cipher is a polyalphabetic substitution cipher, as opposed to the Caesar Cipher which was a monoalphabetic 
+# substitution cipher. What this means is that opposed to having a single shift that is applied to every letter, 
+# the Vigenère Cipher has a different shift for each individual letter. The value of the shift for each letter is determined by a given keyword.
+
+hard_message = "dfc aruw fsti gr vjtwhr wznj? vmph otis! cbx swv jipreneo uhllj kpi rahjib eg fjdkwkedhmp!"
+hard_keyword = "friends"
+
+def new_decoder(message, keyword):
+	letter_pointer = 0
+	keyword_final = ""
+	for i in range(0, len(hard_message)):
+		if hard_message[i] in punct:
+			keyword_final += hard_message[i]	
+		else:
+			keyword_final += keyword[letter_pointer]
+			letter_pointer = (letter_pointer+1) % len(keyword)
+	translate_message = ""
+	for i in range(0, len(hard_message)):
+		if not hard_message[i] in punct:
+			ln = alpha.find(hard_message[i]) - alpha.find(keyword_final[i])
+			translate_message += alpha[ln % len(alpha)]
+		else:
+			translate_message += hard_message[i]
+	return translate_message
+	
+print(new_decoder(hard_message, hard_keyword))
+
+# ok, the above is just way above my head. Not sure if I'm ever able to do stuff like this... Or will i? lol gdi. Tricky stuff. 
+
+	
 
