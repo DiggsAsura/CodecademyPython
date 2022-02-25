48470fdd4e4822cdaea78ec556852a5cc7870daf
DiggsAsura <kenneth.bjerke@gmail.com>
2022-02-25 15:39:43 +0100

    added 10_1_3 and its following file just_the_first.txt

---
+++ 10_1_3_Reading_a_Line.py
@@ -0,0 +1,10 @@
+# Chap 10
+# 1. Learn Python: Files
+# 3. Reading a Line
+
+with open("just_the_first.txt") as first_line_doc:
+	first_line = first_line_doc.readline()
+	second_line = first_line_doc.readline()
+
+print(first_line)
+print(second_line)
