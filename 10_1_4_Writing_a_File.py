a2dcb3cd4d5c0668b1c6b138701faed779462d18
DiggsAsura <kenneth.bjerke@gmail.com>
2022-02-26 10:32:24 +0100

    10_1_4 edited, and 10_1_5 with following file cool_dogs.txt

--- 10_1_4_Writing_a_File.py
+++ 10_1_4_Writing_a_File.py
@@ -10,16 +10,27 @@
 #
 # default argument is "r" (read)
 
-with open("bad_bands.txt", "w") as bad_bands_doc:
-	bad_bands = bad_bands_doc.write("Creed")
+#with open("bad_bands.txt", "w") as bad_bands_doc:
+#	bad_bands = bad_bands_doc.write("Creed")
 
-print(bad_bands) # ok interesting, this prints 5. apparently
+#print(bad_bands) # ok interesting, this prints 5. apparently
 								 # cant to much with the variable, or??
 
 # Guess I can do it like this, but.. should be another
 # way to use this information? Guess just have to proceed
 # with the course. 
-with open("bad_bands.txt", "r") as bad_bands_read:
-	bad_bands_r = bad_bands_read.readline()
+#with open("bad_bands.txt", "r") as bad_bands_read:
+#	bad_bands_r = bad_bands_read.readline()
 	
-print(bad_bands_r)
+#print(bad_bands_r)
+
+# actually, seems like i just have to push both arguments
+# with a r+ (or w+?), both should work for read and write. 
+
+with open("bad_bands.txt", "w+") as bad_bands_doc:
+	bad_bands = bad_bands_doc.write("Creed")
+print(bad_bands_doc.readlines())
+
+# I'm still confused about overwrite or not, and indentation
+# of the print statement. 
+
